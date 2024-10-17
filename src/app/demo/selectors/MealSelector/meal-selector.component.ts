dd2f9886b2ce0cbefdbd26a1c3166e0ead70cd3c
Balázs Gábris <mbair@almaspite.hu>
2024-10-17 15:16:44 +0200

    Set Meals colors

--- src/app/demo/selectors/MealSelector/meal-selector.component.ts
+++ src/app/demo/selectors/MealSelector/meal-selector.component.ts
@@ -9,28 +9,37 @@ import { TranslateService } from '@ngx-translate/core';
 export class MealSelectorComponent {
     @Input() parentForm: FormGroup
     @Input() controlName: string
+    @Input() showClear: boolean
     meals: any[] = []
 
     constructor(private translate: TranslateService) {
         this.setMeals()
 
-        // Nyelvváltás figyelése
+        // Set the meal options when the language changes
         this.translate.onLangChange.subscribe(() => {
             this.setMeals()
         })
     }
 
+    /**
+     * Returns the FormControl object for the meal selector.
+     * @returns {FormControl} the FormControl object
+     */
     getFormControl(): FormControl {
         return this.parentForm.get(this.controlName) as FormControl
     }
 
 
+    /**
+     * Sets the available meal options for the meal selector component.
+     * Translates the meal labels to the current language and maps them to their respective values.
+     */
     setMeals() {
         this.meals = [
-            { label: this.translate.instant('MEALS.BREAKFAST'), value: 'reggeli' },
-            { label: this.translate.instant('MEALS.LUNCH'), value: 'ebéd' },
-            { label: this.translate.instant('MEALS.DINNER'), value: 'vacsora' },
-            { label: this.translate.instant('MEALS.NOTHING'), value: 'Nem kér étkezést' }
+            { label: this.translate.instant('MEALS.BREAKFAST'), value: 'reggeli', style: 'breakfast' },
+            { label: this.translate.instant('MEALS.LUNCH'), value: 'ebéd', style: 'lunch' },
+            { label: this.translate.instant('MEALS.DINNER'), value: 'vacsora', style: 'dinner' },
+            { label: this.translate.instant('MEALS.NOTHING'), value: 'Nem kér étkezést', style: 'nothing' }
         ]
     }
 }
