3fabb9b731ae825ae3581dbe859ce9c332a51f44
Balázs Gábris <mbair@almaspite.hu>
2024-11-07 08:51:36 +0100

    Translate forgotten password page

--- src/app/demo/components/auth/forgotpassword/forgotpassword.module.ts
+++ src/app/demo/components/auth/forgotpassword/forgotpassword.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ForgotPasswordRoutingModule } from './forgotpassword-routing.module';
-import { ForgotPasswordComponent } from './forgotpassword.component';
+import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { AppConfigModule } from 'src/app/layout/config/app.config.module';
 import { MessagesModule } from 'primeng/messages';
+import { AppConfigModule } from 'src/app/layout/config/app.config.module';
+import { ForgotPasswordRoutingModule } from './forgotpassword-routing.module';
+import { ForgotPasswordComponent } from './forgotpassword.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
+        TranslateModule,
         ButtonModule,
         InputTextModule,
         ForgotPasswordRoutingModule,
