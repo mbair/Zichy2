deae6238f580abb66d283db29403bebd46d3e7ce
Balázs Gábris <mbair@almaspite.hu>
2025-07-10 16:06:39 +0200

    EnabledSelector implements ControlValueAccessor

--- src/app/demo/selectors/enabled-selector/enabled-selector.component.ts
+++ src/app/demo/selectors/enabled-selector/enabled-selector.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, Output, SimpleChanges, ChangeDetectorRef, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormControl, FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { DropdownChangeEvent } from 'primeng/dropdown';
 
@@ -10,9 +10,16 @@ export interface changeEvent {
 
 @Component({
     selector: 'app-enabled-selector',
-    templateUrl: './enabled-selector.component.html'
+    templateUrl: './enabled-selector.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => EnabledSelectorComponent),
+            multi: true
+        }
+    ]
 })
-export class EnabledSelectorComponent {
+export class EnabledSelectorComponent implements OnInit, ControlValueAccessor {
     @Input() parentForm: FormGroup
     @Input() controlName: string
     @Input() showClear: boolean
@@ -20,8 +27,10 @@ export class EnabledSelectorComponent {
 
     values: { label: string; value: string }[] = []    // Enabled values
     selectedValue: string                               // Selected value
+    disabled = false
 
-    constructor(private translate: TranslateService) { }
+    constructor(private translate: TranslateService, 
+                private cdRef: ChangeDetectorRef) { }
 
     /**
      * Lifecycle hook: called when the component is initialized.
@@ -32,6 +41,7 @@ export class EnabledSelectorComponent {
         this.translate.onLangChange.subscribe(() => {
             this.setValues()
         })
+        this.setValues()
     }
 
     /**
@@ -74,6 +84,67 @@ export class EnabledSelectorComponent {
      * @param event the change event of the diet selector
      */
     handleOnChange(event: DropdownChangeEvent) {
+        this.selectedValue = event.value
+        this.onChange(event.value)
+        this.onTouched()
         this.change.emit({ value: event.value, field: this.controlName })
     }
+
+    /**
+     * Sets the disabled state of the component.
+     * Used by Angular forms to enable/disable the input dynamically.
+     * 
+     * @param isDisabled - Boolean indicating whether the component should be disabled.
+     */
+    setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled
+        this.cdRef.detectChanges()
+    }
+
+    // ===========================
+    // ControlValueAccessor Methods
+    // ===========================
+
+    /**
+     * Writes the value from the parent form into the component.
+     * Used when the form initializes or updates externally.
+     * 
+     * @param value - The selected conferences coming from the form.
+     */
+    writeValue(value: any): void {
+        this.selectedValue = value
+        this.cdRef.detectChanges()
+    }
+
+    /**
+     * Registers a callback function that is called when the value changes.
+     * This is part of the ControlValueAccessor implementation.
+     * 
+     * @param fn - The callback function to be triggered on value change.
+     */
+    registerOnChange(fn: any): void {
+        this.onChange = fn
+    }
+
+    /**
+     * Registers a callback function that is called when the input is touched.
+     * This is part of the ControlValueAccessor implementation.
+     * 
+     * @param fn - The callback function to be triggered on input touch.
+     */
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn
+    }
+
+    /**
+     * Callback function to handle value changes from the parent form.
+     * Initially set as an empty function, but will be assigned dynamically.
+     */
+    onChange = (_: any) => { }
+
+    /**
+     * Callback function to handle when the input is touched.
+     * Initially set as an empty function, but will be assigned dynamically.
+     */
+    onTouched = () => { }
 }
