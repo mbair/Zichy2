166c79f7944fb0a140660e520b09883c90bede91
Balázs Gábris <mbair@almaspite.hu>
2026-03-16 19:09:30 +0100

    Sync generated frontend assets after version bump

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.15.111";
-export const APP_BUILD_TIME = "2026-03-16T18:01:12.132Z";
+export const APP_VERSION = "1.15.112";
+export const APP_BUILD_TIME = "2026-03-16T18:08:29.955Z";
