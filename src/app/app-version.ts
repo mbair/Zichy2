498228a91d097502a135a0ff6c8c2020dcf1d833
Balázs Gábris <mbair@almaspite.hu>
2026-03-16 19:26:04 +0100

    chore: bump frontend version to 1.16.0

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.15.112";
-export const APP_BUILD_TIME = "2026-03-16T18:08:29.955Z";
+export const APP_VERSION = "1.16.0";
+export const APP_BUILD_TIME = "2026-03-16T18:22:02.338Z";
