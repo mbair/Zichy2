45a51caa3b4dcee4b781209a9ccf7e05b31b3eb8
Balázs Gábris <mbair@almaspite.hu>
2026-03-20 16:26:34 +0100

    Fix: Calendar defaultDate in conference-form (beginDate - endDate)

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.16.6";
-export const APP_BUILD_TIME = "2026-03-20T15:15:41.947Z";
+export const APP_VERSION = "1.16.7";
+export const APP_BUILD_TIME = "2026-03-20T15:24:10.052Z";
