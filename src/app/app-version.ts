a4b99515e5bfbe200836884372a9e8c4df2fb601
Balázs Gábris <mbair@almaspite.hu>
2026-03-24 17:00:09 +0100

    Organizer dashboard improvement

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.16.27";
-export const APP_BUILD_TIME = "2026-03-24T15:49:21.561Z";
+export const APP_VERSION = "1.16.28";
+export const APP_BUILD_TIME = "2026-03-24T15:58:50.190Z";
