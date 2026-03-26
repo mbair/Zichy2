cd5c915119ed2fdaf074a1ffc2019863d8701a0c
Balázs Gábris <mbair@almaspite.hu>
2026-03-26 07:25:54 +0100

    fix(dashboard): Organizer dashboard text change

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.16.38";
-export const APP_BUILD_TIME = "2026-03-26T06:18:47.761Z";
+export const APP_VERSION = "1.16.39";
+export const APP_BUILD_TIME = "2026-03-26T06:24:53.662Z";
