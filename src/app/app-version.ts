6c2a923ef0339ae76704354bcd4413fd6000e1d8
Balázs Gábris <mbair@almaspite.hu>
2026-03-29 17:03:00 +0200

    Improve log timestamp and long response rendering

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.17.3";
-export const APP_BUILD_TIME = "2026-03-29T14:41:07.527Z";
+export const APP_VERSION = "1.17.4";
+export const APP_BUILD_TIME = "2026-03-29T15:01:17.240Z";
