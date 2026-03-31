7b63ae48fa844f8326fe1f2a9111586656d73933
Balázs Gábris <mbair@almaspite.hu>
2026-03-31 16:23:43 +0200

    Add automatic orphan conference cleanup for legacy guests and log

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.17.7";
-export const APP_BUILD_TIME = "2026-03-31T08:41:43.153Z";
+export const APP_VERSION = "1.17.8";
+export const APP_BUILD_TIME = "2026-03-31T14:19:00.036Z";
