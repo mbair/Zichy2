a9fe7985ccd7c3fbb7a7aa42cfff863bcabacbc7
Balázs Gábris <mbair@almaspite.hu>
2026-04-01 09:49:50 +0200

    feat(room-binder): add room export with shared excel format

--- src/app/app-version.ts
+++ src/app/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.17.11";
-export const APP_BUILD_TIME = "2026-04-01T07:41:11.583Z";
+export const APP_VERSION = "1.17.12";
+export const APP_BUILD_TIME = "2026-04-01T07:47:41.474Z";
